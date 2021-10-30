79f0d2ff8058f3fb36ba749c33bab3d53dcb1bff
Romzi Farhan <romzifg@gmail.com>
2021-10-30 10:19:20 +0700

    add api mentor & courses

--- service-course/app/Models/Mentor.php
+++ service-course/app/Models/Mentor.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Mentor extends Model
 {
     protected $table = "mentors";
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s'
+    ];
+    
     protected $fillable = [
         'name', 'profile', 'email', 'profession'
     ];
--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\MentorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('mentors', [MentorController::class, 'index']);
+Route::get('mentors/{id}', [MentorController::class, 'show']);
+Route::post('mentors', [MentorController::class, 'create']);
+Route::put('mentors/{id}', [MentorController::class, 'update']);
+Route::delete('mentors/{id}', [MentorController::class, 'destroy']);
+
+Route::get('courses', [CourseController::class, 'index']);
+Route::post('courses', [CourseController::class, 'create']);
+Route::put('courses/{id}', [CourseController::class, 'update']);
+Route::delete('courses/{id}', [CourseController::class, 'destroy']);
